526032e004d36a66d096c5e3fa501a1eef9db280
wlindant <106821473+wlindant@users.noreply.github.com>
2025-05-03 11:16:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,25 @@ clock = pygame.time.Clock()
 fruit = FRUIT()
 snake = SNAKE()
 
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SCREEN_UPDATE:
+            snake.move_snake()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snake.direction = Vector2(0,-1)
+            if event.key == pygame.K_DOWN:
+                snake.direction = Vector2(0,1)
+            if event.key == pygame.K_LEFT:
+                snake.direction = Vector2(-1,0)
+            if event.key == pygame.K_RIGHT:
+                snake.direction = Vector2(1,0)
     
     screen.fill((175,215,70))
     fruit.draw_fruit()
