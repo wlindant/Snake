2b588e53af3964d94653ccee76f4170cf47aae00
wlindant <106821473+wlindant@users.noreply.github.com>
2025-05-09 19:40:16 +0200

    Dodanie grafiki węża dla poszczególnych ruchów

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class SNAKE:
 
     def draw_snake(self):
         self.update_head_graphics()
+        self.update_tail_graphics()
 
         for index, block in enumerate(self.body):
             x_pos = int(block.x * cell_size)
@@ -34,16 +35,43 @@ class SNAKE:
             block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
 
             if index == 0:
-                screen.blit(self.head_right, block_rect)
+                screen.blit(self.head, block_rect)
+
+            elif index == len(self.body) - 1:
+                screen.blit(self.tail,block_rect)
+
             else:
-                pygame.draw.rect(screen,(150,100,100),block_rect)
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical,block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal,block_rect)
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        screen.blit(self.body_tl,block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.body_bl,block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.body_tr,block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        screen.blit(self.body_br,block_rect)
+
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1,0): self.tail = self.tail_left
+        elif tail_relation == Vector2(-1,0): self.tail = self.tail_right
+        elif tail_relation == Vector2(0,1): self.tail = self.tail_up
+        elif tail_relation == Vector2(0,-1): self.tail = self.tail_down
+
 
     def update_head_graphics(self):
-        head_relation = self.body[1] = self.body[0]
+        head_relation = self.body[1] - self.body[0]
         if head_relation == Vector2(1,0): self.head = self.head_left
-        elif head_relation == Vector2(-1,0): self.head = self.head_left
-        elif head_relation == Vector2(0,1): self.head = self.head_left
-        elif head_relation == Vector2(0,-1): self.head = self.head_left
+        elif head_relation == Vector2(-1,0): self.head = self.head_right
+        elif head_relation == Vector2(0,1): self.head = self.head_up
+        elif head_relation == Vector2(0,-1): self.head = self.head_down
 
 
 
